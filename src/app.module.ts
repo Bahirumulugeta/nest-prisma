916957b1dbdd7ade062e62ffa4bb7729d7b6e6b3
bahiru-kemer <bmulugeta@kifiya.et>
2023-08-14 11:30:11 +0300

    Fix prettier error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlogController } from './blog/blog.controller';
 import { BlogService } from './blog/blog.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, BlogController],
-  providers: [AppService, BlogService],
+  controllers: [BlogController],
+  providers: [BlogService],
 })
 export class AppModule {}
